b58baaaa7e1bc9aea0567c2b14d0733ab759bc85
FreddieITA91 <36641810+FreddieITA91@users.noreply.github.com>
2026-06-02 16:22:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'schrodingerfridge-step8-v2';
+const CACHE_NAME = 'schrodingerfridge-step9-v1';
 self.addEventListener('install', event => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil((async()=>{
   const keys = await caches.keys();
